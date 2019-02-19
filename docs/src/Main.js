2e2098cdc9f5c0ce3edbbee377da58db4dea53d2
Jeremy CHAUVIN <jeremychauvin10@gmail.com>
2019-02-19 11:09:50 +0100

    ui

--- docs/src/Main.js
+++ docs/src/Main.js
@@ -1,6 +1,6 @@
-export { player } from "./player.js";
-export { monster } from "./monster.js";
-export { Battle } from "./Actor.js";
+import { player } from "./class/player.js";
+import { monster } from "./class/monster.js";
+import { Battle } from "./class/Actor.js";
 
 export function main(){
     alert("CA MARCHE BLYAT !!")
