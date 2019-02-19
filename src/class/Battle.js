84c6d986d26e944ea036af8bc7461807047de640
Axel-White <soulofolbivion@hotmail.fr>
2019-02-19 09:46:17 +0100

    Update Battle.js

--- src/class/Battle.js
+++ src/class/Battle.js
@@ -1,7 +1,9 @@
 import { Field } from "./Field.js";
 export class Battle {
-    constructor(Field) {
+    constructor(Field, players, monters) {
       this.Field = Field;
+      this.players = players;
+      this.monters = monters;
     }
   
     methode(type) {}
