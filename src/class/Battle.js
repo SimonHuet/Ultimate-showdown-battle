c9570013551a00d4801b9e087f345bc863db2f12
Pierre Nègre <pierre.negre@epsi.fr>
2019-02-19 09:55:46 +0100

    ajout de batlle et field (#5)
    * add Battle & Field
    
    * Update Battle.js

---
+++ src/class/Battle.js
@@ -0,0 +1,10 @@
+import { Field } from "./Field.js";
+export class Battle {
+    constructor(Field, players, monters) {
+      this.Field = Field;
+      this.players = players;
+      this.monters = monters;
+    }
+  
+    methode(type) {}
+  }
